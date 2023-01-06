2b94d945410940edca302427972a7498fa8bc971
kottip1759 <122080916+kottip1759@users.noreply.github.com>
2023-01-06 11:15:07 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1672992825,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/карта1ур.png",
+		"images/пол-sheet0.png",
+		"images/дикорация-sheet0.png",
+		"images/персонаж-sheet0.png",
+		"images/персонаж-sheet1.png",
+		"images/персонаж-sheet2.png",
+		"images/coins-sheet0.png",
+		"images/паркур2-sheet0.png",
+		"images/паркур-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/карта2ур-sheet0.png",
+		"images/лества-sheet0.png",
+		"images/алмазныйблок-sheet0.png",
+		"images/замшеныйбулыжник-sheet0.png",
+		"images/блокдревесины-sheet0.png",
+		"images/меню-sheet0.png",
+		"images/новаяигра-sheet0.png",
+		"images/выход-sheet0.png",
+		"images/sprite2-sheet0.png"
+	]
+}
